15149d5dff90ac11e019698104ccd9a70886dca0
韩昊 <178337111@qq.com>
2020-03-26 16:11:29 +0800

    test two

--- 2019102935韩昊/namedtuple_test.py
+++ 2019102935韩昊/namedtuple_test.py
@@ -7,7 +7,10 @@ birth = namedtuple("birth", ["wcalender", "ccalender"])
 people_one = user._make([name('张', '三'), '男', birth('1','10')])
 people_two = user._make([name('李', '四'), '女', birth('2','20')])
 people_three = user._make([name('王', '五'), '男', birth('3','30')])
-userlist = [people_one, people_two, people_three]
+personName = name._make('张', '六')
+birthday = birth._make('4','40')
+people_four = user._make([personName, '男', birthday])
+userlist = [people_one, people_two, people_three,people_four]
 #创建一个userlist
 
 for i in userlist:
