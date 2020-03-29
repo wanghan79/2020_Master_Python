0685987b27cec2d2113176fdcbbb268f78abb4a1
zoubin2019102976 <zoub605@nenu.edu.cn>
2020-03-29 23:43:11 +0800

    second commit

--- 2019102976邹斌/assign.py
+++ 2019102976邹斌/assign.py
@@ -1,17 +1,17 @@
 # !python3.8
-# assign.py: tuple example
-zhangsan = ({'name': ('张三', '小张')}, {'age' : ('1996-05-06', '1992-08-10')})
-print('张三的大名: %s, 张三的小名: %s.' %(zhangsan[0]['name'][0], zhangsan[0]['name'][1]))
-print('张三的生日:')
-print('公历: %s, 农历: %s.' %(zhangsan[1]['age'][0], zhangsan[1]['age'][1]))
-print()
-liming = ({'name': ('李明', '小李')}, {'age' : ('1995-08-16', '1995-07-21')})
-print('李明的大名: %s, 李明的小名: %s.' %(liming[0]['name'][0], liming[0]['name'][1]))
-print('李明的生日:')
-print('公历: %s, 农历: %s.' %(liming[1]['age'][0], liming[1]['age'][1]))
-print()
-hanmeimei = ({'name': ('韩梅梅', '小韩')}, {'age' : ('1997-09-06', '1997-08-05')})
-print('韩梅梅的大名: %s, 韩梅梅的小名: %s.' %(hanmeimei[0]['name'][0], hanmeimei[0]['name'][1]))
-print('韩梅梅的生日:')
-print('公历: %s, 农历: %s.' %(hanmeimei[1]['age'][0], hanmeimei[1]['age'][1]))
+# assign.py: namedtuple example
+from collections import namedtuple
+Students = namedtuple('Students', ['name', 'age'])
 
+list_students = []
+
+zhangsan = Students._make([{'name':'张三', 'nickname':'小张'}, {'G_calendar':'1996-06-16', 'L_calendar':'1996-05-01'}])
+list_students.append(zhangsan)
+liming = Students(name = {'name':'李明', 'nickname':'小李'}, age = {'G_calendar':'1997-06-10', 'L_calendar':'1997-05-06'})
+list_students.append(liming)
+hanmeimei = Students._make([{'name':'韩梅梅', 'nickname':'小韩'}, {'G_calendar':'1998-06-12', 'L_calendar':'1998-05-08'}])
+list_students.append(hanmeimei)
+
+for student in list_students:
+    print('学生的大名: %s, 小名: %s.' %(student.name['name'], student.name['nickname']))
+    print('学生的生日(公历): %s, 农历: %s' %(student.age['G_calendar'], student.age['L_calendar']))
