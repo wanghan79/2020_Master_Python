306f6bb6970c62f1d2d98999365a1d475ec09604
zoubin2019102976 <zoub605@nenu.edu.cn>
2020-03-31 16:17:26 +0800

    3th work

---
+++ 2019102976邹斌/assign02.py
@@ -0,0 +1,49 @@
+#!python3.8
+#assign02.py: generate 1000 numbers of float and 1000 strings
+
+#random.uniform(a, b) 生成随机浮点数, a <= N <=b for a <= b and b<=N<=a for b < a
+#random.randint(a, b) 生成随机整数 a <= N <= b
+
+import random
+
+lenOfNum = 1000
+lenOfStr = 1000
+contents = set()
+letters = []
+def randow_num():               #生成1000个随机数,并加入set
+    global contents
+    for i in range(1000):
+        num = random.uniform(0, 100)
+        contents.add(round(num, 2))
+
+def random_string():     #生成1000个随机字符串,由[a-zA-Z]组成,并加入set
+    global letters
+    global contents
+    l = len(letters) - 1
+    for i in range(1000):
+        letter_len = random.randint(0, 10)
+        str = ''
+        for i in range(letter_len):
+            index = random.randint(0,  l)
+            str += letters[index]
+        contents.add(str)
+
+def create_table():             #生成字母表[a-zA-Z]
+    global letters #test
+    for i in range(65, 91):
+        letters.append(chr(i))
+    for i in range(97, 123):
+        letters.append(chr(i))
+
+create_table()
+randow_num()
+random_string()
+
+for item in contents:
+    if str(item).isalpha():
+        if 'at' in item:
+            print('The target string is %s' % (item))
+    elif item != '':
+        if 20 <= float(item) <= 50:
+            print('The target number is %s' % (item))
+
