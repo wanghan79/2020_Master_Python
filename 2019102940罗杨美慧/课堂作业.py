a0bc28038d46bbedbd63da156e98d85257dbb69c
罗杨美慧-2019102940 <54993052+luoyangmeihui-2019102940@users.noreply.github.com>
2020-03-31 20:23:55 +0800

    Update 课堂作业.py

--- 2019102940罗杨美慧/课堂作业.py
+++ 2019102940罗杨美慧/课堂作业.py
@@ -4,24 +4,24 @@ def randNum():
     s1 = []
 
     for i in range(0,1000):
-        num = random.randint(0,100)
+        num = random.uniform(0,100)
         if num >= 20 and num <= 50:
             s1.append(num)
 
-    print('[20,50]之间的数字集合为：',s1,end=" ")
+    print('[20,50]之间的数字集合为：',s1,end = " ")
 
 def randStr():
     s2 = []
 
     for i in range(1000):
         s = ''
-        for j in range(5):
-            s += random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+        for j in range(8):
+            s += random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890')
         s2.append(s)
 
     for i in s2:
         if 'at' in i:
-            print(i, end='  ')
+            print(i, end = '  ')
 
 #主函数
 if __name__ == '__main__':
