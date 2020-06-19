5bee444dcca880d15214b560dc9f81218e29130e
张业勤 <57352072+zhangyeqin2019102975@users.noreply.github.com>
2020-06-19 23:58:26 +0800

    Create jieba.py

---
+++ 张业勤2019102975/jieba.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import jieba
+from collections import Counter
+
+datelist = ['03_31']
+wordlist = ''
+for i in range(len(datelist)):
+#     filename = datelist[i]
+    data = pd.read_csv(datelist[i]+'_11.csv',encoding="gbk").iloc[:,1:]
+    df=pd.DataFrame(data)
+    a=df["热搜标题"].tolist()
+    b=','.join([str(i) for i in a])
+    wordlist+=b
+# wordlist
+
+cut_words = ""
+temp = jieba.cut_for_search(wordlist)
+cut_words += (" ".join(temp))
+all_words=cut_words.split()
+while ',' in all_words:
+    all_words.remove(',')#replace不好使
+counter = Counter(all_words)
+dictionary=dict(counter)
+df = pd.DataFrame.from_dict(dictionary, orient='index',columns=['词频'])
+df = df.reset_index().rename(columns = {'index':'热搜词'})
+dff = df.sort_values(by="词频" , ascending=False)
+dff.to_csv("./热搜分词/68.csv",encoding="gbk",index=None)
